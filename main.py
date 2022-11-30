58f32a323f92fecc9955c4fbe436631f52cf8788
xing-angela <angela_xing@brown.edu>
2022-11-29 23:56:19 -0500

    shapenet dataset loader and preprocessing

---
+++ main.py
@@ -0,0 +1,54 @@
+import argparse
+import utils.data_loader as data_loader
+import matplotlib.pyplot as plt
+import random
+import tensorflow as tf
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Parse Data for Pix2Vox")
+
+    parser.add_argument("--dataset", help="the dataset to use")
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    # sets up the data loader
+    train_data_loader = data_loader.DATASET_MAPPINGS["ShapeNet"](args)
+    dataset = train_data_loader.load_dataset(data_loader.DatasetType.TRAIN, 1)
+    dataset.set_data()
+
+    images = dataset.images
+
+    augment_prep_fn = tf.keras.Sequential(
+        [
+            tf.keras.layers.RandomCrop(width=224, height=224),
+            tf.keras.layers.RandomFlip(),
+            tf.keras.layers.RandomRotation(factor=0.05)
+        ]
+    )
+
+    rand_indices = random.sample(range(0, len(images)), 10)
+    rand_imgs = [images[i] for i in rand_indices]
+    augmented = augment_prep_fn(tf.convert_to_tensor(rand_imgs))
+
+    # fig = plt.figure()
+
+    # for i in range(1, len(rand_imgs)+1):
+    #     fig.add_subplot(5, 5, i)
+    #     plt.imshow(augmented[i-1])
+    # plt.show()
+
+    fig, ax = plt.subplots(2, 10)
+    for i in range(1, len(rand_imgs)+1):
+        ax[1][i-1].imshow(images[rand_indices[i-1]])
+        ax[0][i-1].imshow(augmented[i-1])
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils/data_loader.py
@@ -0,0 +1,159 @@
+import json
+import os
+import random
+import cv2
+import numpy as np
+import sys
+from datetime import datetime as dt
+from enum import Enum
+import sys
+import utils.binvox_rw
+import tensorflow as tf
+
+
+class DatasetType(Enum):
+    TRAIN = 0
+    TEST = 1
+    VAL = 2
+
+
+class ShapeNetDataset:
+    def __init__(self, dataset_type, data_files, n_views_rendering):
+        self.dataset_type = dataset_type
+        self.data_files = data_files
+        self.n_views_rendering = n_views_rendering
+        self.images = []
+        self.volumes = []
+
+    def set_data(self):
+        for i in range(len(self.data_files)):
+            taxonomy_name, split_name, images, volume = self.get_datum(i)
+            self.images.extend(images)
+            self.volumes.extend(volume)
+
+        print('[INFO] %s Complete collecting saving images and volumes of dataset.' % (
+            dt.now()))
+
+    def get_datum(self, idx):
+        taxonomy_name = self.data_files[idx]["taxonomy_name"]
+        split_name = self.data_files[idx]["split_name"]
+        image_paths = self.data_files[idx]["image_paths"]
+        volume_path = self.data_files[idx]["volume_path"]
+
+        # get the image renderings
+        if self.dataset_type == DatasetType.TRAIN:
+            selected_rendering_paths = [
+                image_paths[i]
+                for i in random.sample(range(len(image_paths)), self.n_views_rendering)
+            ]
+        else:
+            selected_rendering_paths = [
+                image_paths[i] for i in range(self.n_views_rendering)]
+
+        images = []
+        for path in selected_rendering_paths:
+            img = cv2.imread(path, cv2.IMREAD_UNCHANGED).astype(
+                np.float32) / 255.
+
+            if len(img.shape) < 3:
+                print('[FATAL] %s It seems that there is something wrong with the image file %s' %
+                      (dt.now(), path))
+                sys.exit(2)
+
+            images.append(img)
+
+        # get the volume rendering
+        # ext = os.path.splitext(volume_path)[1]
+        # if ext == ".binvox":
+        with open(volume_path, 'rb') as f:
+            volume = utils.binvox_rw.read_as_3d_array(f)
+            volume = volume.data.astype(np.float32)
+
+        return (taxonomy_name, split_name, np.asarray(images), volume)
+
+
+class ShapeNetDataLoader:
+    def __init__(self, args):
+        self.dataset_taxonomy = None
+        self.image_path = "data/ShapeNet/ShapeNetRendering"
+        self.volume_path = "data/ShapeNet/ShapeNetVox32"
+
+        with open("datasets/ShapeNet.json", encoding='utf-8') as file:
+            self.dataset_taxonomy = json.loads(file.read())
+
+    def load_dataset(self, dataset_type, n_views_rendering):
+        data_files = []
+
+        tax_shortened = [self.dataset_taxonomy[i] for i in range(3)]
+
+        # load the data for each taxonomy (aka each category)
+        for taxonomy in tax_shortened:
+            folder_name = taxonomy["taxonomy_id"]
+
+            print('[INFO] %s Collecting files of Taxonomy[ID=%s, Name=%s]' %
+                  (dt.now(), taxonomy['taxonomy_id'], taxonomy['taxonomy_name']))
+
+            splits = []
+
+            if dataset_type == DatasetType.TRAIN:
+                splits = taxonomy["train"]
+            elif dataset_type == DatasetType.VAL:
+                splits = taxonomy["val"]
+            elif dataset_type == DatasetType.TEST:
+                splits = taxonomy["test"]
+
+            # adds each of the taxonomy files to the list of data files
+            taxonomy_files = self.get_files_for_taxonomy(
+                folder_name, splits)
+            data_files.extend(taxonomy_files)
+
+            print('[INFO] %s Complete collecting files of the dataset. Total files: %d.' % (
+                dt.now(), len(data_files)))
+        return ShapeNetDataset(dataset_type, data_files, n_views_rendering)
+
+    def get_files_for_taxonomy(self, taxonomy_folder_name, splits):
+        data_files = []
+
+        for split in splits:
+            vol_path = os.path.join(
+                self.volume_path, taxonomy_folder_name, split, "model.binvox")
+            if not os.path.exists(vol_path):
+                print('[WARN] %s Ignore sample %s/%s since volume file does not exist.' %
+                      (dt.now(), taxonomy_folder_name, split))
+                continue
+
+            # get file paths for images
+            img_paths = []
+            img_file_path = os.path.join(
+                self.image_path, taxonomy_folder_name, split, "rendering")
+            views = sorted(os.listdir(img_file_path))
+            # the folder also contains rendering.txt and metadata (which we don't want)
+            views = views[:len(views)-2]
+
+            for view in views:
+                curr_file_path = os.path.join(
+                    self.image_path, taxonomy_folder_name, split, "rendering", view)
+                if not os.path.exists(curr_file_path):
+                    continue
+
+                img_paths.append(curr_file_path)
+
+            if len(img_paths) == 0:
+                print('[WARN] %s Ignore sample %s/%s since image files do not exist.' %
+                      (dt.now(), taxonomy_folder_name, split))
+                continue
+
+            # append the paths for the taxonomy to the output
+            data_files.append({
+                "taxonomy_name": taxonomy_folder_name,
+                "split_name": split,
+                "image_paths": img_paths,
+                "volume_path": vol_path
+            })
+
+        return data_files
+
+
+DATASET_MAPPINGS = {
+    "ShapeNet": ShapeNetDataLoader
+}
