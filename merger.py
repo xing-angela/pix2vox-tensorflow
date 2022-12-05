b7997d6320f3d589b3a8929194628224c59343f2
melahi5 <77821142+melahi5@users.noreply.github.com>
2022-12-04 21:17:34 -0500

    work

--- merger.py
+++ merger.py
@@ -4,40 +4,66 @@ class Merger(tf.keras.layers.Layer):
 
     def __init__(self, cfg):
         super(Merger, self).__init__()
+        self.cfg = cfg
 
-        self.layer1 = tf.keras.Sequential()
-        self.layer1.add(tf.keras.layers.Conv3D(9, 16, kernel_size=3, padding=1))
-        self.layer1.add(tf.keras.layers.BatchNormalization())
-        self.layer1.add(tf.keras.layers.LeakyReLU(0.2))
+        self.layer1 = tf.keras.Sequential([
+            tf.keras.layers.Conv3D(9, 16, kernel_size=3, padding=1),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.LeakyReLU(0.2)
+        ])
+        # self.layer1 = tf.keras.Sequential()
+        # self.layer1.add(tf.keras.layers.Conv3D(9, 16, kernel_size=3, padding=1))
+        # self.layer1.add(tf.keras.layers.BatchNormalization())
+        # self.layer1.add(tf.keras.layers.LeakyReLU(0.2))
 
-        self.layer2 = tf.keras.Sequential()
-        self.layer2.add(tf.keras.layers.Conv3D(16, 8, kernel_size=3, padding=1))
-        self.layer2.add(tf.keras.layers.BatchNormalization())
-        self.layer2.add(tf.keras.layers.LeakyReLU(0.2))
+        self.layer2 = tf.keras.Sequential([
+            tf.keras.layers.Conv3D(16, 8, kernel_size=3, padding=1),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.LeakyReLU(0.2)
+        ])
+        # self.layer2 = tf.keras.Sequential()
+        # self.layer2.add(tf.keras.layers.Conv3D(16, 8, kernel_size=3, padding=1))
+        # self.layer2.add(tf.keras.layers.BatchNormalization())
+        # self.layer2.add(tf.keras.layers.LeakyReLU(0.2))
 
-        self.layer3 = tf.keras.Sequential()
-        self.layer3.add(tf.keras.layers.Conv3D(8,4, kernel_size=3, padding=1))
-        self.layer3.add(tf.keras.layers.BatchNormalization())
-        self.layer3.add(tf.keras.layers.LeakyReLU(0.2))
+        self.layer3 = tf.keras.Sequential([
+            tf.keras.layers.Conv3D(8, 4, kernel_size=3, padding=1),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.LeakyReLU(0.2)
+        ])
+        # self.layer3 = tf.keras.Sequential()
+        # self.layer3.add(tf.keras.layers.Conv3D(8,4, kernel_size=3, padding=1))
+        # self.layer3.add(tf.keras.layers.BatchNormalization())
+        # self.layer3.add(tf.keras.layers.LeakyReLU(0.2))
 
-        self.layer4 = tf.keras.Sequential()
-        self.layer4.add(tf.keras.layers.Conv3D(4,2, kernel_size=3, padding=1))
-        self.layer4.add(tf.keras.layers.BatchNormalization())
-        self.layer4.add(tf.keras.layers.LeakyReLU(0.2))
+        self.layer4 = tf.keras.Sequential([
+            tf.keras.layers.Conv3D(4, 2, kernel_size=3, padding=1),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.LeakyReLU(0.2)
+        ])
+        # self.layer4 = tf.keras.Sequential()
+        # self.layer4.add(tf.keras.layers.Conv3D(4,2, kernel_size=3, padding=1))
+        # self.layer4.add(tf.keras.layers.BatchNormalization())
+        # self.layer4.add(tf.keras.layers.LeakyReLU(0.2))
 
-        self.layer5 = tf.keras.Sequential()
-        self.layer5.add(tf.keras.layers.Conv3D(4,2, kernel_size=3, padding=1))
-        self.layer5.add(tf.keras.layers.BatchNormalization())
-        self.layer5.add(tf.keras.layers.LeakyReLU(0.2))
+        self.layer5 = tf.keras.Sequential([
+            tf.keras.layers.Conv3D(4, 2, kernel_size=3, padding=1),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.LeakyReLU(0.2)
+        ])
+        # self.layer5 = tf.keras.Sequential()
+        # self.layer5.add(tf.keras.layers.Conv3D(4,2, kernel_size=3, padding=1))
+        # self.layer5.add(tf.keras.layers.BatchNormalization())
+        # self.layer5.add(tf.keras.layers.LeakyReLU(0.2))
 
     
-    def call(self, raw_features, coarse_volumes):
+    def forward(self, raw_features, coarse_volumes):
         n_views_rendering = coarse_volumes.size(1)
         tf.split(raw_features, 1, dim=1)
         volume_weights = []
 
         for i in range(n_views_rendering):
-            raw_feature = tf.squeze(raw_features[i], dim=1)
+            raw_feature = tf.squeeze(raw_features[i], dim=1)
 
             volume_weight = self.layer1(raw_feature)
             volume_weight = self.layer2(volume_weight)
@@ -49,9 +75,8 @@ class Merger(tf.keras.layers.Layer):
             volume_weights.append(volume_weight)
         
         volume_weights = tf.stack(volume_weights).transpose(volume_weights,(1, 0, 2, 3, 4))
-        volume_weights = tf.keras.activations.softmax(volume_weights)
+        volume_weights = tf.nn.softmax(volume_weights)
         coarse_volumes = coarse_volumes * volume_weights
         coarse_volumes = tf.math.reduce_sum(coarse_volumes, axis=1)
         
         return tf.clip_by_value(coarse_volumes, clip_value_min=0, clip_value_max=1)
-        #
--- refiner.py
+++ refiner.py
@@ -3,45 +3,88 @@ import tensorflow as tf
 class Refiner(tf.keras.layers.Layer):
     def __init__(self, cfg):
         super(Refiner, self).__init__()
+        self.cfg = cfg
         
-        self.layer1 = tf.keras.Sequential()
-        self.layer1.add(tf.keras.layers.Conv3D(1, 32, kernel_size=4, padding=2))
-        self.layer1.add(tf.keras.layers.BatchNormalization())
-        self.layer1.add(tf.keras.layers.LeakyReLU(0.2))
-        self.layer1.add(tf.keras.layers.MaxPool3D(pool_size=2))
+        self.layer1 = tf.keras.Sequential([
+            tf.keras.layers.Conv3D(1, 32, kernel_size=4, padding=2),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.LeakyReLU(0.2), # check config.py for this number (0.2); I didn't use cfg.
+            tf.keras.layers.MaxPool3D(pool_size=2)
+        ])
+        # self.layer1 = tf.keras.Sequential()
+        # self.layer1.add(tf.keras.layers.Conv3D(1, 32, kernel_size=4, padding=2))
+        # self.layer1.add(tf.keras.layers.BatchNormalization())
+        # self.layer1.add(tf.keras.layers.LeakyReLU(0.2))
+        # self.layer1.add(tf.keras.layers.MaxPool3D(pool_size=2))
 
-        self.layer2 = tf.keras.Sequential()
-        self.layer2.add(tf.keras.layers.Conv3D(1, 32, kernel_size=4, padding=2))
-        self.layer2.add(tf.keras.layers.BatchNormalization())
-        self.layer2.add(tf.keras.layers.LeakyReLU(0.2))
-        self.layer2.add(tf.keras.layers.MaxPool3D(pool_size=2))
+        self.layer2 = tf.keras.Sequential([
+            tf.keras.layers.Conv3D(32, 64, kernel_size=4, padding=2),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.LeakyReLU(0.2),
+            tf.keras.layers.MaxPool3D(pool_size=2)
+        ])
+        # self.layer2 = tf.keras.Sequential()
+        # self.layer2.add(tf.keras.layers.Conv3D(1, 32, kernel_size=4, padding=2))
+        # self.layer2.add(tf.keras.layers.BatchNormalization())
+        # self.layer2.add(tf.keras.layers.LeakyReLU(0.2))
+        # self.layer2.add(tf.keras.layers.MaxPool3D(pool_size=2))
 
-        self.layer3 = tf.keras.Sequential()
-        self.layer3.add(tf.keras.layers.Conv3D(64, 128, kernel_size=4, padding=2))
-        self.layer3.add(tf.keras.layers.BatchNormalization())
-        self.layer3.add(tf.keras.layers.LeakyReLU(0.2))
-        self.layer3.add(tf.keras.layers.MaxPool3D(pool_size=2))
+        self.layer3 = tf.keras.Sequential([
+            tf.keras.layers.Conv3D(64, 128, kernel_size=4, padding=2),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.LeakyReLU(0.2),
+            tf.keras.layers.MaxPool3D(pool_size=2)
+        ])
+        # self.layer3 = tf.keras.Sequential()
+        # self.layer3.add(tf.keras.layers.Conv3D(64, 128, kernel_size=4, padding=2))
+        # self.layer3.add(tf.keras.layers.BatchNormalization())
+        # self.layer3.add(tf.keras.layers.LeakyReLU(0.2))
+        # self.layer3.add(tf.keras.layers.MaxPool3D(pool_size=2))
+        
+        self.layer4 = tf.keras.Sequential([
+           tf.keras.layers.Dense(2048, input_shape=(8192,), activation=None), 
+           tf.keras.layers.ReLU()
+        ])
+        # self.layer4 = tf.keras.Sequential()
+        # self.layer4.add(tf.keras.layers.Dense(2048, input_shape=(8192,), activation=None))
+        # self.layer4.add(tf.keras.layers.ReLU())
 
-        self.layer4 = tf.keras.Sequential()
-        self.layer4.add(tf.keras.layers.Dense(2048, input_shape=(8192,), activation=None))
-        self.layer4.add(tf.keras.layers.ReLU())
+        self.layer5 = tf.keras.Sequential([
+           tf.keras.layers.Dense(8192, input_shape=(2048,), activation=None), 
+           tf.keras.layers.ReLU()
+        ])
+        # self.layer5 = tf.keras.Sequential()
+        # self.layer5.add(tf.keras.layers.Dense(8192, input_shape=(2048,), activation=None))
+        # self.layer5.add(tf.keras.layers.ReLU())
 
-        self.layer5 = tf.keras.Sequential()
-        self.layer5.add(tf.keras.layers.Dense(8192, input_shape=(2048,), activation=None))
-        self.layer5.add(tf.keras.layers.ReLU())
+        self.layer6 = tf.keras.Sequential([
+           tf.keras.layers.Conv3DTranspose(128, 64, kernel_size=4, stride=2, use_bias=False, padding=1),
+           tf.keras.layers.BatchNormalization(),
+           tf.keras.layers.ReLU()
+        ])
+        # self.layer6 = tf.keras.Sequential()
+        # self.layer6.add(tf.keras.layers.Conv3DTranspose(128, 64, kernel_size=4, stride=2, use_bias=False, padding=1))
+        # self.layer6.add(tf.keras.layers.BatchNormalization())
+        # self.layer6.add(tf.keras.layers.ReLU())
 
-        self.layer6 = tf.keras.Sequential()
-        self.layer6.add(tf.keras.layers.Conv3DTranspose(128, 64, kernel_size=4, stride=2, use_bias=False, padding=1))
-        self.layer6.add(tf.keras.layers.BatchNormalization())
-        self.layer6.add(tf.keras.layers.ReLU())
+        self.layer7 = tf.keras.Sequential([
+           tf.keras.layers.Conv3DTranspose(64, 32, kernel_size=4, stride=2, use_bias=False, padding=1),
+           tf.keras.layers.BatchNormalization(),
+           tf.keras.layers.ReLU()
+        ])
+        # self.layer7 = tf.keras.Sequential()
+        # self.layer7.add(tf.keras.layers.Conv3DTranspose(64, 32, kernel_size=4, stride=2, use_bias=False, padding=1))
+        # self.layer7.add(tf.keras.layers.BatchNormalization())
+        # self.layer7.add(tf.keras.layers.ReLU())
 
-        self.layer7 = tf.keras.Sequential()
-        self.layer7.add(tf.keras.layers.Conv3DTranspose(64, 32, kernel_size=4, stride=2, use_bias=False, padding=1))
-        self.layer7.add(tf.keras.layers.BatchNormalization())
-        self.layer7.add(tf.keras.layers.ReLU())
+        self.layer8 = tf.keras.Sequential([
+           tf.keras.layers.Conv3DTranspose(32, 1, kernel_size=4, stride=2, use_bias=False, padding=1),
+           tf.nn.sigmoid()
+        ])
 
-    def call(self, coarse_volumes):
-        volumes_32_l = coarse_volumes.view((-1, 1,32, 32, 32))
+    # NOTE: NOT SURE ABOUT .view(); maybe reshape? 
+    def forward(self, coarse_volumes):
+        volumes_32_l = coarse_volumes.view((-1, 1,32, 32, 32)) # tf.reshape(coarse_volumes, (-1, 1,32, 32, 32))???? OR tf.keras.layers.reshape((-1, 1,32, 32, 32))
         volumes_16_l = self.layer1(volumes_32_l)
         volumes_8_l = self.layer2(volumes_16_l)
         volumes_4_l = self.layer3(volumes_8_l)
@@ -53,4 +96,3 @@ class Refiner(tf.keras.layers.Layer):
         volumes_32_r = (volumes_32_l + self.layer8(volumes_16_r)) * 0.5
 
         return volumes_32_r.view((-1,32,32,32))
-#
