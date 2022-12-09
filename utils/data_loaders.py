07988b86c37e685915b78c832a8dfac6dc2673c3
hannahstein10 <77816252+hannahstein10@users.noreply.github.com>
2022-12-09 12:11:38 -0500

    Update data_loaders.py

--- utils/data_loaders.py
+++ utils/data_loaders.py
@@ -163,6 +163,142 @@ class ShapeNetDataLoader:
 
 #################################### END SHAPENET ####################################
 
+class Pix3dDataset:
+    def __init__(self, files):
+        self.file_list = files
+
+        self.taxonomy_names = []
+        self.sample_names = []
+        self.images = []
+        self.volumes = []
+        self.bounding_boxes = []
+
+        # load the data
+        self.load_data()
+
+    def load_data(self):
+        for file in self.file_list:
+            # Saves the taxonomy names and sample names
+            taxonomy_name = file['taxonomy_name']
+            sample_name = file['sample_name']
+            self.taxonomy_names.append(taxonomy_name)
+            self.sample_names.append(sample_name)
+
+            image_paths = file['image_paths']
+            volume_path = file['volume_path']
+
+            bounding_box = file['bounding_box']
+            self.bounding_boxes.append(bounding_box)
+
+            # Get image renderings
+            rendering_images = []
+            for path in image_paths:
+                rendering_image = cv2.imread(path, cv2.IMREAD_UNCHANGED).astype(np.float32)/255.
+                if len(rendering_image.shape) <3:
+                    print('[WARN] %s It seems the image file %s is grayscale.' % (dt.now(), path))
+                    rendering_image = np.stack((rendering_image, )*3, -1)
+
+                rendering_images.append(rendering_image)
+
+            # Get data of volume
+            with open(volume_path, 'rb') as f:
+                volume = utils.binvox_rw.read_as_3d_array(f)
+                volume = volume.data.astype(np.float32)
+
+            self.volumes.append(volume)
+
+        self.images = np.asarray(rendering_images)
+        self.volumes = np.asarray(self.volumes)
+
+        print('[INFO] %s Complete loading files of the dataset.' % (dt.now()))
+
+        # return taxonomy_name, sample_name, rendering_images, self.volumes, self.bounding_boxes
+
+class Pix3dDataLoader:
+    def __init__(self, cfg):
+        self.dataset_taxonomy = None
+        self.annotations = dict()
+        self.volume_path_template = cfg.DATASETS.PIX3D.VOXEL_PATH
+        self.rendering_image_path_template = cfg.DATASETS.PIX3D.RENDERING_PATH
+
+        # Load all taxonomies of the dataset
+        with open(cfg.DATASETS.PIX3D.TAXONOMY_FILE_PATH, encoding='utf-8') as file:
+            self.dataset_taxonomy = json.loads(file.read())
+
+        # Load all annotations of the dataset
+        _annotations = None
+        with open(cfg.DATASETS.PIX3D.ANNOTATION_PATH, encoding='utf-8') as file:
+            _annotations = json.loads(file.read())
+
+        for anno in _annotations:
+            filename, _ = os.path.splitext(anno['img'])
+            anno_key = filename[4:]
+            self.annotations[anno_key] = anno
+
+    def load_dataset_files(self, dataset_type, n_views_rendering, transforms=None):
+        files = []
+
+        # Load data for each category
+        for taxonomy in self.dataset_taxonomy:
+            taxonomy_name = taxonomy['taxonomy_name']
+            print('[INFO] %s Collecting files of Taxonomy[Name=%s]' % (dt.now(), taxonomy_name))
+
+            samples = []
+            if dataset_type == 'train':
+                samples = taxonomy['train']
+            elif dataset_type == 'test':
+                samples = taxonomy['test']
+            elif dataset_type == 'val':
+                samples = taxonomy['test']
+
+            files.extend(self.get_files_of_taxonomy(taxonomy_name, samples))
+
+        print('[INFO] %s Complete collecting files of the dataset. Total files: %d.' % (dt.now(), len(files)))
+        return Pix3dDataset(files, transforms)
+
+    def get_files_of_taxonomy(self, taxonomy_name, samples):
+        files_of_taxonomy = []
+
+        for sample_idx, sample_name in enumerate(samples):
+            # Get image annotations
+            anno_key = '%s/%s' % (taxonomy_name, sample_name)
+            annotations = self.annotations[anno_key]
+
+            # Get file list of rendering images
+            _, img_file_suffix = os.path.splitext(annotations['img'])
+            rendering_image_file_path = self.rendering_image_path_template % (taxonomy_name, sample_name,
+                                                                              img_file_suffix[1:])
+
+            # Get the bounding box of the image
+            img_width, img_height = annotations['img_size']
+            bbox = [
+                annotations['bbox'][0] / img_width,
+                annotations['bbox'][1] / img_height,
+                annotations['bbox'][2] / img_width,
+                annotations['bbox'][3] / img_height
+            ]  # yapf: disable
+            model_name_parts = annotations['voxel'].split('/')
+            model_name = model_name_parts[2]
+            volume_file_name = model_name_parts[3][:-4].replace('voxel', 'model')
+
+            # Get file path of volumes
+            volume_file_path = self.volume_path_template % (taxonomy_name, model_name, volume_file_name)
+            if not os.path.exists(volume_file_path):
+                print('[WARN] %s Ignore sample %s/%s since volume file not exists.' %
+                      (dt.now(), taxonomy_name, sample_name))
+                continue
+
+            # Append to the list of rendering images
+            files_of_taxonomy.append({
+                'taxonomy_name': taxonomy_name,
+                'sample_name': sample_name,
+                'rendering_image': rendering_image_file_path,
+                'bounding_box': bbox,
+                'volume': volume_file_path,
+            })
+
+        return files_of_taxonomy
+
 
 DATASET_LOADER_MAPPING = {
     'ShapeNet': ShapeNetDataLoader,
